a698a57f52bc66f08e344a7e826f080e75d43bb3
sincerelyyyash <yta0088@gmail.com>
2025-09-15 16:15:12 +0530

    webhook init

---
+++ apps/server/src/controllers/webhook.controller.ts
@@ -0,0 +1,14 @@
+import type { Request, Response } from "express";
+
+export const webhookHandler = async (req: Request, res: Response) => {
+
+  // const { success, data } = webhookHandlerSchema.safeParse(req.body);
+  // if (!success || !data) {
+  //   return res.status(411).json({
+  //     message: "Invalid inputs"
+  //   })
+  // }
+  //
+
+
+}
